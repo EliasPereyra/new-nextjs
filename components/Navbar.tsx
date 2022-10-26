ef5bc243a614a24fa5cc09893be1f82b1a7ed0da
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-10-25 22:30:41 -0300

    Creating a navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.elements}>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/contact">
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
