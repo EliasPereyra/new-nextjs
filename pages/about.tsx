e0c4043bedd0435280a3d90234d119dd3fc6250f
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-10-25 22:31:32 -0300

    Making an about page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function about(): JSX.Element {
+  return (
+    <div id="about">
+      <Navbar />
+      <h1>About </h1>
+    </div>
+  );
+}
+
+export default about;
