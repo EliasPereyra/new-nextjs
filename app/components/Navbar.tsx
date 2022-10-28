1a3609f39556feedac2344219669f32f2a77ce5f
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-10-28 08:42:41 -0300

    Creating the app dir experimental of nextjs 13

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import styles from "../styles/Navbar.module.css";
+import styles from "../../styles/Navbar.module.css";
 
 function Navbar() {
   return (
